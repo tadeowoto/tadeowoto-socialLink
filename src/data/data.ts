db0727355b7d479139b13812e13497fbf6aca5f0
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-01-06 17:21:35 -0300

    adding data and assets

---
+++ src/data/data.ts
@@ -0,0 +1,26 @@
+
+const socialLinks = [
+    {
+        id: 1,
+        url: "https://www.github.com/tadeowoto",
+        text: "GitHub"
+    },
+    {
+        id: 2,
+        url: "https://x.com/Tade_woto",
+        text: "X"
+    },
+    {
+        id: 3,
+        url: "https://www.discord.com/tadiu_",
+        text: "Discord"
+    },
+    {
+        id: 4,
+        url: "https://www.linkedin.com/in/tadeowoto/",
+        text: "Linkedin"
+
+    },
+]
+
+export default socialLinks
